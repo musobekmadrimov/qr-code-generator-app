eec705033a093025f7d1b42f5414fc68d3efd68e
musobekmadrimov <musobekmadrimov99@gmail.com>
2022-01-14 06:47:26 +0200

    finished project..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [temp, setTemp] = useState("");
+  const [word, setWord] = useState("");
+  const [qrCode, setQrCode] = useState(" ");
+  const [bgColor, setBgColor] = useState("");
+
+  useEffect(() => {
+    setQrCode(
+      `http://api.qrserver.com/v1/create-qr-code/?data=${word}&size${""}&bgcolor=${bgColor}`
+    );
+  }, [word, bgColor]);
+
+  function handleGenerator() {
+    setWord(temp);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <h1 className="themeText">React QR Code Generator</h1>
+      <div className="inputSec">
+        <input
+          type="text"
+          placeholder="Enter Anything You Want..."
+          className="placeInput"
+          onChange={(e) => setTemp(e.target.value)}
+        />
+      </div>
+      {!qrCode ? (
+        <div className="result">
+        
+      </div>
+      ) : (
+        <div className="result">
+          <img src={qrCode} alt="" />
+        </div>
+      )}
+
+      <div className="buttonsArea">
+        <button className="generateBtn" onClick={handleGenerator}>
+          Generate
+        </button>
+        <a href={qrCode} download className="downloadBtn">
+          Download
         </a>
-      </header>
-    </div>
+        <input
+          type="color"
+          onChange={(e) => setBgColor(e.target.value.substring(1))}
+        />
+      </div>
+    </>
   );
 }
 
